5742bf2f5600c64a9e8a1c2a1a99a3311a283f6c
YAXI KANANI <yaxikanani1812@gmail.com>
2025-10-06 17:04:33 +0530

    new

---
+++ js/diamond.js
@@ -0,0 +1,13 @@
+const products = [
+  {
+    img: 'necklace1.jpg',
+    title: 'Silver Plated AD Cocktail Necklace',
+    stars: 4,
+    price: 233,
+    orig: 424,
+    off: 45
+  },
+  // ...more products
+];
+
+// ...rendering function (similar structure as above)
